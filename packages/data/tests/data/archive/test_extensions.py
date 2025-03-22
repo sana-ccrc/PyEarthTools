5c1e210f8426a3db2988d66fb1e4d91330776bc0
Tennessee Leeuwenburg <tennessee.leeuwenburg@bom.gov.au>
2025-03-22 20:36:12 +1100

    Add test coverage for the register_archive function

---
+++ packages/data/tests/data/archive/test_extensions.py
@@ -0,0 +1,30 @@
+import pytest
+
+from pyearthtools.data.archive import extensions
+import pyearthtools.data
+
+
+def test_register_archive():
+
+
+    @extensions.register_archive("NewData")
+    class NewData:
+         def __init__(self, args):
+             self.args = args             
+
+
+    # If registering over the top of an existing name, confirm warning occurs
+    with pytest.warns(pyearthtools.data.AccessorRegistrationWarning):
+        @extensions.register_archive("NewData")
+        class NewData:
+
+            _pyearthtools_initialisation = {"class":  {}}
+
+            def __init__(self, args):
+                self.args = args
+
+            def __call__(self, *args, **kwargs):
+                pass
+
+
+
