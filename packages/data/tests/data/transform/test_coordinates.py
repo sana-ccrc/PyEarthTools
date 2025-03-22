ce295f9ed47973edec520fef7b6893d03dec368a
Tennessee Leeuwenburg <tennessee.leeuwenburg@bom.gov.au>
2025-03-22 14:59:37 +1100

    Add some more coordinate tests

--- packages/data/tests/data/transform/test_coordinates.py
+++ packages/data/tests/data/transform/test_coordinates.py
@@ -4,21 +4,18 @@ import numpy as np
 import pytest
 
 
+# Test data, re-used across tests
+lon180 = list(range(-180, 180))
+lon360 = list(range(0, 360))
+lon_unclear = list(range(10, 120))
+data = list(range(-180, 180))
+data_unclear = list(range(10, 120))
+da180 = xr.DataArray(coords={"longitude": lon180}, dims=["longitude"])
+da360 = xr.DataArray(coords={"longitude": lon360}, dims=["longitude"])
+da_wrongname = xr.DataArray(coords={"longname": lon180}, dims=["longname"])
+da_unclear = xr.DataArray(coords={"longitude": lon_unclear}, dims=["longitude"])
+
 def test_get_longitude():
-
-    lon180 = list(range(-180, 180))
-    lon360 = list(range(0, 360))
-    lon_unclear = list(range(10, 120))
-    data = list(range(-180, 180))
-    data_unclear = list(range(10, 120))
-
-    da180 = xr.DataArray(coords={"longitude": lon180}, dims=["longitude"])
-
-    da360 = xr.DataArray(coords={"longitude": lon360}, dims=["longitude"])
-
-    da_wrongname = xr.DataArray(coords={"longname": lon180}, dims=["longname"])
-
-    da_unclear = xr.DataArray(coords={"longitude": lon_unclear}, dims=["longitude"])
 
     longitude_type = coordinates.get_longitude(da180, transform=False)
     assert longitude_type == "-180-180"
@@ -36,3 +33,20 @@ def test_get_longitude():
 
     with pytest.raises(ValueError):
         result = coordinates.get_longitude(da_unclear, transform=False)
+
+def test_StandardLongitude():
+
+	conform = coordinates.StandardLongitude("0-360")
+	fixed = conform.apply(da180)
+	assert fixed is not None
+	unchanged = conform.apply(da360)
+	# TODO - shouldn't this be true?
+	# assert xr.testing.assert_equal(fixed, da360)
+
+	conform = coordinates.StandardLongitude("-180-180")
+	fixed = conform.apply(da360)
+	unchanged = conform.apply(da180)
+	assert fixed is not None	
+	# TODO - shouldn't this be true?
+	# assert xr.testing.assert_equal(fixed, da180)	
+
