785e53f09d5da269b7280551220866c28cb3c729
Tennessee Leeuwenburg <tennessee.leeuwenburg@bom.gov.au>
2025-04-05 21:27:51 +1100

    Uplift coverage of xarray/chunk.py to 100%

---
+++ packages/pipeline/tests/operations/xarray/test_chunk.py
@@ -0,0 +1,31 @@
+# Copyright Commonwealth of Australia, Bureau of Meteorology 2025.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pyearthtools.pipeline.operations.xarray.chunk import Chunk
+
+import numpy as np
+import xarray as xr
+
+def test_Chunk():
+
+    c = Chunk()
+
+    data = np.ones((50, 50))
+    da = xr.DataArray(coords = {"a": list(range(50)), "b": list(range(50))}, data=data)
+
+    da2 = c.apply_func(da)
+    da3 = c.undo_func(da2)
+
+    xr.testing.assert_equal(da3, da)
+
